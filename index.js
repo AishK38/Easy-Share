0ef65b3499967b1e70e5a70cdb269376ce1c11c7
Aishwarya Karmakar <80018881+AishK38@users.noreply.github.com>
2022-10-05 08:38:00 +0530

    style: added progress bar

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const dropzone = document.querySelector(".drop-zone");
 const fileinput = document.querySelector("#fileinp");
 const browsebtn = document.querySelector(".browse");
+const host = "";
+const uploadurl = `${host}api/files`;
+const bgProgress = document.querySelector(".bg-progress");
 
 dropzone.addEventListener("dragover", (e)=>{
     e.preventDefault();
@@ -22,3 +25,29 @@ dropzone.addEventListener("drop", (e)=>{
 browsebtn.addEventListener("click", ()=>{
   fileinput.click();
 });
+
+fileinput.addEventListener("change", () => {
+  uploadfile();
+});
+
+const uploadfile = () => {
+    const file = fileinput.files[0];
+    const formData = new FormData();
+    formData.append("myfile", file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === XMLHttpRequest.DONE) {  
+        console.log(xhr.response);
+      }  
+    };
+    xhr.upload.onprogress = updateProgress;
+    xhr.open("POST", uploadurl);
+    xhr.send(formData);
+};
+
+const updateProgress = (e) => {
+    const perc = Math.round((e.loaded / e.total) * 100);
+    console.log(perc);
+    bgProgress.style.width = `${perc}%`;
+}
